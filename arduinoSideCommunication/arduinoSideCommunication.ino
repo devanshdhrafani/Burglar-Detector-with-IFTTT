c52b43ababb29538548463b814c6e5f06f2ba0eb
devanshdhrafani <43034785+devanshdhrafani@users.noreply.github.com>
2018-09-29 16:26:36 +0530

    Add files via upload

---
+++ arduinoSideCommunication/arduinoSideCommunication.ino
@@ -0,0 +1,18 @@
+const int pinSensor = 8; //pin number for PIR sensor
+
+void setup() 
+{
+  pinMode(pinSensor, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int sensorValue = digitalRead(pinSensor); 
+
+  if(sensorValue == 1)    //will send '1' or '2' value to Serial which will be read by the python script
+    Serial.println('1');
+  else
+    Serial.println('2');
+
+}
